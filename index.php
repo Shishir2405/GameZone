e43acd6c4f6f5e34686501bf3ed0f586aa17a6b1
Shishir Shrivastava <shishirshrivastava30@gmail.com>
2025-05-28 23:22:49 +0530

    Remove nixpacks cache

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 ob_start();
 session_start();
+include('include/connection.php'); // Make sure this is included first
 ?>
 <!doctype html>
 <html lang="en">
@@ -8,109 +9,76 @@ session_start();
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <?php include'head.php' ?>
-<link rel="stylesheet" href="assets/css/style.css?v=2">
+<link rel="stylesheet" href="assets/css/style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="Bitter Mobile Template">
+<meta name="keywords" content="bootstrap, mobile template, bootstrap 4, mobile, html, responsive" />
 
 <style>
-.pleft {
-	padding-left:3px;
-}
-.pright {
-	padding-right:3px;
-}
-
-.vcard {
-  background: #fff;
-  border-radius: 10px;
-}
-
-.vcard {
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-}
-
-.card:hover {
-  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-
-  .height{ height:40px; line-height:40px;}
-.height .pageTitle{ line-height:2em;}
-
+    .btn { 
+        background-image: linear-gradient(#29B6F6, #29B6F6);
+        border-radius: 5px 5px 5px 5px;
+        border: 0.5px solid white;
+        color: white;
+        transition: 0.5s;
+    }
 </style>
 </head>
 
 <body>
-<?php include("include/connection.php");?>
-<!-- Page loading -->
 
-<!-- * Page loading --> 
-<a href="#">
- 
-<div class="appHeader1 height">
-  <div class="text-center"> 
-    <div class="pageTitle"><i class="icon ion-md-download"></i> Download App</div>
-  </div>
-  
-  </div>
-</a>
 <!-- App Header -->
-<div class="appHeader">
-  <div class="pageTitle"  > <h1 style="color:#ffb700; font-size: 26px; font-family: arial;">2XClub.SHOP</h1></div>
+<div class="appHeader1" style="box-shadow:none;background-color:#ffb700 !important">
+    <div class="left">
+        <a href="javascript:void(0);" class="icon goBack">&nbsp;</a>
+        <div class="pageTitle">Login</div>
+    </div>
 </div>
+<!-- * App Header --> 
 
 <!-- App Capsule -->
-<div id="appCapsule"> 
-  
-  
-  <div class="appContent">
-    <div class="sectionTitle mb-2"> 
-      
-      <!-- post list -->
-      <div class="vcard">
-        <div class="row"> 
-          <!-- item -->
-          <?php
-	$i=0;
-  $productQuery=mysqli_query($con,"select * from `tbl_product` where `status`='1'");
-  while($productResult=mysqli_fetch_array($productQuery)){$i++;?>
-          <div class="col-6 <?php if(($i%2)==0){echo"pleft";}else{echo "pright";}?>">
-            <div class="vcard card"> <a href="productdetails.php?pid=<?php echo encryptor('encrypt', $productResult['id']); ?>" class="postItem">
-              <div class="imageWrapper"> <img src="<?php echo $productResult['images'];?>" alt="image" class="image"> </div>
-              <p class="text-center"><?php echo $productResult['name'];?></p>
-              <footer> ₹ <?php echo number_format($productResult['price'], 2);?></footer>
-              </a> </div>
-          </div>
-          <?php }?>
-          <!-- * item --> 
-          <!-- item -->
-              <div class="p-3"></div>
-      
-      
-      <!-- * listview --> 
-      
+<div id="appCapsule">
+    <div class="appContent1">
+        <form action="#" id="loginForm" method="post" class="card-body" autocomplete="off">
+            <div class="inner-addon left-addon">
+                <i class="icon ion-md-phone-portrait"></i>
+                <input type="tel" id="login_mobile" name="login_mobile" class="form-control" placeholder="Mobile Number" onKeyPress="return isNumber(event)" maxlength="10">
+            </div>
+            <div class="inner-addon left-addon">
+                <i class="icon ion-md-key"></i>
+                <input type="password" id="login_password" name="login_password" class="form-control" placeholder="Password">
+            </div>
+            <input type="hidden" name="action" value="login">
+            <div>
+                <div class="text-center mt-3">
+                    <button type="submit" class="btn btn-light" style="width:264px;"> Login </button>
+                </div>
+                <div class="text-center mt-2">
+                    <a href="signup.php" class="btn btn-light">Sign Up</a>
+                    <a href="forgot-password.php" class="btn btn-light">Reset Password</a>
+                </div>
+            </div>
+        </form>
     </div>
-        
-        
-        </div>
-      </div>
-      
-      <!-- * post list -->
-      
-    
-    <!-- app Footer --> 
-    
-    <!-- * app Footer --> 
-    
-  </div>
-  
 </div>
-
-<!-- * appCapsule -->
+<!-- appCapsule -->
 
 <?php include("include/footer.php");?>
 
-<!-- ///////////// Js Files ////////////////////  --> 
-<!-- Jquery --> 
+<div id="registertoast" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content ">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <div class="text-center" id="regtoast">          
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="assets/js/lib/jquery-3.4.1.min.js"></script> 
 <!-- Bootstrap--> 
 <script src="assets/js/lib/popper.min.js"></script> 
@@ -119,5 +87,19 @@ session_start();
 <script src="assets/js/plugins/owl.carousel.min.js"></script> 
 <!-- Main Js File --> 
 <script src="assets/js/app.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/account.js"></script>
+
+<script>
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+</script>
+
 </body>
 </html>
